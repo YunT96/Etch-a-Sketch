76f3ce4c69d1857a934f18e3ab8962d015ffd696
YunT96 <163807078+YunT96@users.noreply.github.com>
2024-05-31 17:47:00 +0800

    Add UI to allow user to change grid size
    - Add UI to take get a number from 1 to 100 from user
    - Add function to take that value, calculate and change grid size

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
 console.log("Script Loaded");
 
 const gridContainer = document.querySelector(".grid-container");
+const sizeInput = document.querySelector(".size-input");
+const sizeBtn = document.querySelector(".size-btn");
 
-let boxNum = 256;
+sizeBtn.addEventListener("click", getGridSize);
+createGrid(16);
 
-for (let i = 0; i < boxNum; i++){
-    const box = document.createElement("div");
-    box.classList.add("box");
-    gridContainer.appendChild(box);
+function createGrid(squaresPerSide){
+    gridContainer.innerHTML = ''; // Clear existing grid
+    const boxSize = 960 / squaresPerSide; // Calculate the size of each box
+    for (let i = 0; i < squaresPerSide* squaresPerSide; i++){
+        const box = document.createElement("div");
+        box.classList.add("box");
+        box.style.height = (`${boxSize}px`);
+        box.style.width = (`${boxSize}px`);
 
-    box.addEventListener("mouseover", () =>{
-        box.classList.add("hover");
-    });
+        gridContainer.appendChild(box);
+    
+        box.addEventListener("mouseover", () =>{
+            box.classList.add("hover");
+        });
+    }
+}
+
+function getGridSize(){
+    let squaresPerSide = parseInt(sizeInput.value);
+
+    if (squaresPerSide && squaresPerSide > 0 && squaresPerSide <=100){
+        createGrid(squaresPerSide);
+    }
+    else{
+        alert("Enter a number between 1 - 100");
+    }
 }
 
 
