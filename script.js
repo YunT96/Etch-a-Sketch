1c807dfe46c508c003dc504bf1e9d4095ad29217
YunT96 <163807078+YunT96@users.noreply.github.com>
2024-05-31 16:19:50 +0800

    Setup grid and logic to change color
    -Set up initial grid with size 16x16
    -Add event listener to change grid color when hovered over

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 console.log("Script Loaded");
+
+const gridContainer = document.querySelector(".grid-container");
+
+let boxNum = 256;
+
+for (let i = 0; i < boxNum; i++){
+    const box = document.createElement("div");
+    box.classList.add("box");
+    gridContainer.appendChild(box);
+
+    box.addEventListener("mouseover", () =>{
+        box.classList.add("hover");
+    });
+}
+
+
+
+
+
+
+
+
