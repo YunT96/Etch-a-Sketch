8846dc7779230bde16f93a3e4f1459414c631802
YunT96 <163807078+YunT96@users.noreply.github.com>
2024-06-03 15:09:09 +0800

    Add random color functionality

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function createGrid(squaresPerSide){
         gridContainer.appendChild(box);
     
         box.addEventListener("mouseover", () =>{
-            box.classList.add("hover");
+            box.style.backgroundColor = getRandomColor();
         });
     }
 }
@@ -35,8 +35,14 @@ function getGridSize(){
     }
 }
 
-
-
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
 
 
 
